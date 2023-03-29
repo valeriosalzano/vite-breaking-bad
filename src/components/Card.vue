ffbbc1ee366c230527407074e91b0a5aa990529c
Valerio Salzano <vlr.slzn@gmail.com>
2023-03-29 23:04:46 +0200

    changed card component style

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="card text-bg-dark text-center pb-5 h-100 position-relative">
+  <div class="card h-100 position-relative bg-transparent border-dark">
     <img class="img-fluid" :src="cardData.card_images[0].image_url" :alt="cardData.name">
-    <h3 class="my-3">{{ cardData.name }}</h3>
-    <h5 class="position-absolute bottom-0 start-50 translate-middle">{{ cardData.archetype }}</h5>
+    <div class="card-text text-bg-dark text-center position-relative px-3 pb-5 h-100 rounded-bottom">
+      <h3 class="py-3">{{ cardData.name }}</h3>
+      <h5 class="position-absolute bottom-0 start-50 translate-middle">{{ cardData.archetype }}</h5>
+    </div>
   </div>
 </template>
 
