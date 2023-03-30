2868868b7f1af25321b214e3ce56ee66b2830180
Valerio Salzano <vlr.slzn@gmail.com>
2023-03-30 10:46:58 +0200

    moved sass to dev in package.json added computed elements to card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="card h-100 position-relative bg-transparent border-dark">
-    <img class="img-fluid" :src="cardData.card_images[0].image_url" :alt="cardData.name">
+    <img class="img-fluid" :src="computedImg" :alt="computedName">
     <div class="card-text text-bg-dark text-center position-relative px-3 pb-5 h-100 rounded-bottom">
-      <h3 class="py-3">{{ cardData.name }}</h3>
-      <h5 class="position-absolute bottom-0 start-50 translate-middle">{{ cardData.archetype }}</h5>
+      <h3 class="py-3">{{ computedName }}</h3>
+      <h5 class="position-absolute bottom-0 start-50 translate-middle">{{ computedArchetype }}</h5>
     </div>
   </div>
 </template>
@@ -13,6 +13,17 @@ export default {
   name: 'Card',
   props: {
     cardData : Object,
+  },
+  computed: {
+    computedImg(){
+      return this.cardData.card_images[0].image_url;
+    },
+    computedName(){
+      return this.cardData.name;
+    },
+    computedArchetype(){
+      return this.cardData.archetype;
+    }
   }
 }
 </script>
