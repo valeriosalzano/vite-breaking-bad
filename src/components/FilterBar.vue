045752d0392c3c444d7502ef4dcc6467a98ccea0
Valerio Salzano <vlr.slzn@gmail.com>
2023-03-30 18:40:14 +0200

    labels added to select

--- src/components/FilterBar.vue
+++ src/components/FilterBar.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="filter-bar container d-flex justify-content-between">
-    <select class="form-select text-bg-dark" id="filter-bar-archetype" v-model="store.searchFilter.archetype" @change="$emit('filterChangeArchetype')">
-      <option value=""> All Archetypes </option>
-      <option v-for="archetype in store.cardsArchetypes" :value="archetype.archetype_name">{{ archetype.archetype_name }}</option>
-    </select>
+  <div class="filter-bar container d-flex justify-content-between align-items-center">
+    <div class="input-group">
+      <label for="filter-bar-archetype" class="input-group-text text-bg-dark">Archetype:</label>
+      <select class="form-select text-bg-dark" id="filter-bar-archetype" v-model="store.searchFilter.archetype" @change="$emit('filterChangeArchetype')">
+        <option value=""> All Archetypes </option>
+        <option v-for="archetype in store.cardsArchetypes" :value="archetype.archetype_name">{{ archetype.archetype_name }}</option>
+      </select>
+    </div>
 
-    <select class="form-select text-bg-dark" id="filter-bar-limit" v-model="store.searchFilter.num" @change="$emit('filterChangeLimit')">
-      <option value="24"> 24 </option>
-      <option value="30"> 30 </option>
-      <option value="50"> 50 </option>
-      <option value="100"> 100 </option>
-    </select>
+    <div class="input-group">
+      <label for="filter-bar-limit" class="input-group-text text-bg-dark">Results limit:</label>
+      <select class="form-select text-bg-dark" id="filter-bar-limit" v-model="store.searchFilter.num" @change="$emit('filterChangeLimit')">
+        <option value="24"> 24 </option>
+        <option value="30"> 30 </option>
+        <option value="50"> 50 </option>
+        <option value="100"> 100 </option>
+      </select>
+    </div>
 
   </div>
 </template>
